1c60383a8119092efae2a244ac78ad71d8c7b684
Dylan Eilber <dylaneilber@gmail.com>
2025-10-01 15:42:40 -0700

    need to do logic for getting each players best hand

--- server/card.go
+++ server/card.go
@@ -43,6 +43,65 @@ const (
 	StraightFlush HandType = "straight flush"
 )
 
+func isStraightFlush(h Hand, p Player) bool {
+
+}
+
+func isQuads(h Hand, p Player) bool {
+	// if either of the players cards has freq of 4 then return true
+	if getCardFrequencies(h, p)[p.hand[0].Rank] == 4 || getCardFrequencies(h, p)[p.hand[1].Rank] == 4 {
+		return true
+	}
+	return false
+}
+
+func isFullHouse(h Hand, p Player) bool {
+
+}
+
+func isFlush(h Hand, p Player) bool {
+
+}
+
+func isStraight(h Hand, p Player) bool {
+
+}
+
+func isThreeOfAKind(h Hand, p Player) bool {
+
+}
+
+func isTwoPair(h Hand, p Player) bool {
+
+}
+
+func isPair(h Hand, p Player) bool {
+
+}
+
+func getCardFrequencies(h Hand, p Player) map[string]int {
+
+	freqs := make(map[string]int)
+	for i := range h.board {
+		freqs[h.board[i].Rank]++
+	}
+	for i := range p.hand {
+		freqs[p.hand[i].Rank]++
+	}
+	return freqs
+}
+
+func getSuitFrequencies(h Hand, p Player) map[string]int {
+	freqs := make(map[string]int)
+	for i := range h.board {
+		freqs[h.board[i].Suit]++
+	}
+	for i := range p.hand {
+		freqs[p.hand[i].Suit]++
+	}
+	return freqs
+}
+
 func getPlayerBestHand(h Hand, p Player) HandType {
 
 }
